3c6829ac67d614a26368b7122304523c43e9c41e
jhonatanalmanza <jhoalmanza52@gmail.com>
2025-09-20 08:30:38 -0500

    code updated for to work with anode commond display

--- exercie4.X/exercise4.c
+++ exercie4.X/exercise4.c
@@ -1,25 +1,26 @@
 #include <xc.h>
 
-#pragma config FOSC = HS 
-#pragma config WDTE = OFF 
-#pragma config PWRTE = OFF 
+#pragma config FOSC = HS
+#pragma config WDTE = OFF
+#pragma config PWRTE = OFF
 #pragma config MCLRE = ON
-#pragma config CP = OFF 
-#pragma config CPD = OFF 
-#pragma config BOREN = OFF 
+#pragma config CP = OFF
+#pragma config CPD = OFF
+#pragma config BOREN = OFF
 #pragma config IESO = OFF
 #pragma config FCMEN = OFF
-#pragma config LVP = OFF 
-#pragma config DEBUG = OFF 
+#pragma config LVP = OFF
+#pragma config DEBUG = OFF
 
 #define _XTAL_FREQ 20000000
 #define TIEMPO 40
 
+// Matriz de dígitos para displays de ánodo común
+const unsigned char DIGITS[10] = {0xC0, 0xF9, 0xA4, 0xB0, 0x99, 0x92, 0x82, 0xF8, 0x80, 0x90};
 
-const unsigned char DIGITS[10] = {0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F};
 unsigned char count = 0;
 int value = 0;
-unsigned char state = 0; // 0 paused , 1 started
+unsigned char state = 1; // 0 paused , 1 started
 
 void show_numbers(int val) {
     int dec = 0;
@@ -29,18 +30,16 @@ void show_numbers(int val) {
     uni = (val%10);
 
     PORTC = DIGITS[uni];
-    PORTD = 0x02;
+    PORTD = 0xFD; // 1111 1101b - Pone a D1 en LOW
     __delay_ms(TIEMPO);
-    PORTD = 0x00;
-
+    
     PORTC = DIGITS[dec];
-    PORTD = 0x01;
+    PORTD = 0xFE; // 1111 1110b - Pone a D0 en LOW
     __delay_ms(TIEMPO);
-    PORTD = 0x00;
 }
 
 void listener_btns() {
-    // Button 1 START and RENAUDER 
+    // Button 1 START
     if (RA0 == 0) {
         __delay_ms(50);
         if (RA0 == 0) {
@@ -49,7 +48,7 @@ void listener_btns() {
         }
     }
 
-    // Button 2 PAUSE 
+    // Button 2 PAUSE
     if (RA1 == 0) {
         __delay_ms(50);
         if (RA1 == 0) {
@@ -68,11 +67,9 @@ void listener_btns() {
             while (RA2 == 0);
         }
     }
-
 }
 
 int main(void) {
-
     // set up PORTC
     PORTC = 0x00;
     TRISC = 0x00;
@@ -87,7 +84,6 @@ int main(void) {
     TRISA = 0x07;
 
     while (1) {
-
         listener_btns();
         show_numbers(value);
 
@@ -103,7 +99,5 @@ int main(void) {
             }
         }
     }
-
     return 0;
-
 }
